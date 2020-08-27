c9a3afecc076071e43580e589b4d89aafd4e08bb
Abijith Kattangeri <abijith.kattangeri@gramener.org>
2020-08-27 16:47:55 +0530

    Updated

--- Deleting Folders/FoldersDelete.py
+++ Deleting Folders/FoldersDelete.py
@@ -4,7 +4,7 @@ import shutil
 """
 Path for the folder
 """
-directory = 'D:\\'
+directory = 'D:\\check folder - Copy'
 
 
 """
@@ -18,13 +18,9 @@ old = now - 24 * 60 * 60
 """
 Loops through the folder and checks for the subfolders
 """
-for filename in os.listdir(directory):
-	Main_folder_path = os.path.join(directory, filename) # Joining the foldername to the path
-	stat_result = os.stat(Main_folder_path) # getting the stats of the folder
 
-
-	if stat_result.st_ctime < old: # checking the time of the creation of the folder if it is not created today
-		try:
-			shutil.rmtree(Main_folder_path) # Removes the folders which are older than a day.
-		except:
-			continue
+subfolders = [os.path.join(directory, folder) for folder in os.listdir(directory) if os.path.isdir(os.path.join(directory,folder))]
+for i in subfolders:
+	stat_result = os.stat(i)
+	if stat_result.st_mtime < old:
+		shutil.rmtree(i)
